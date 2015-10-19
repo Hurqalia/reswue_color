38cb2af25b2e3805b785ee967a03be27505c9a7d
Hurqalia <Hurqalia@users.noreply.github.com>
2015-10-19 18:06:12 +0200

    Create reswue-color.meta.js

---
+++ reswue-color.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @id             iitc-plugin-reswue_colors
+// @name           IITC plugin: ResWue Change links color
+// @author         hurqalia22
+// @category       Info
+// @version        0.1.2.20150801.001
+// @namespace      https://github.com/Hurqalia/reswue_colors
+// @updateURL      https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.meta.js
+// @downloadURL    https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.user.js
+// @installURL     https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.user.js
+// @description    [hurqalia22-2015-08-01-001] Reswue Change links color
+// @include        https://www.ingress.com/intel*
+// @include        http://www.ingress.com/intel*
+// @match          https://www.ingress.com/intel*
+// @match          http://www.ingress.com/intel*
+// @grant          none 
+// ==/UserScript==

ba46ba80cb6ea0450fe4e7219f768e45987fa8fd
Hurqalia <Hurqalia@users.noreply.github.com>
2015-10-24 14:56:45 +0200

    Update reswue_colors.user.js

--- reswue_colors.user.js
+++ reswue_colors.user.js
@@ -3,7 +3,7 @@
 // @name           IITC plugin: ResWue Change links color
 // @author         hurqalia22
 // @category       Info
-// @version        0.1.4.20150801.001
+// @version        0.1.5.20150801.001
 // @namespace      https://github.com/Hurqalia/reswue_colors
 // @updateURL      https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.meta.js
 // @downloadURL    https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.user.js

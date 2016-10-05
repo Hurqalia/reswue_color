9e29f275f6eedafe16c827b3824ba633ef1ed28b
Hurqalia <Hurqalia@users.noreply.github.com>
2016-10-05 13:33:06 +0200

    Update reswue_colors.user.js
    Fix Niantics redirect

--- reswue_colors.user.js
+++ reswue_colors.user.js
@@ -9,11 +9,14 @@
 // @downloadURL    https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.user.js
 // @installURL     https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.user.js
 // @description    [hurqalia22-2015-08-01-001] Reswue Change links color
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
-// @grant          none 
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // ==/UserScript==              
                                 
 function wrapper(plugin_info) {

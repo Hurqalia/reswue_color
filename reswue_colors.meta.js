ec7384b520f076b54369db5c85e14a713327f29f
Hurqalia <Hurqalia@users.noreply.github.com>
2016-10-05 13:32:48 +0200

    Update reswue_colors.meta.js
    Fix Niantics redirect

--- reswue_colors.meta.js
+++ reswue_colors.meta.js
@@ -3,15 +3,19 @@
 // @name           IITC plugin: ResWue Change links color
 // @author         hurqalia22
 // @category       Info
-// @version        0.1.11.20150801.001
+// @version        0.1.12.20150801.001
 // @namespace      https://github.com/Hurqalia/reswue_colors
 // @updateURL      https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.meta.js
 // @downloadURL    https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.user.js
 // @installURL     https://github.com/Hurqalia/reswue_colors/raw/master/reswue_colors.user.js
 // @description    [hurqalia22-2015-08-01-001] Reswue Change links color
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission/*
+// @include        http://*.ingress.com/mission/*
+// @match          https://*.ingress.com/mission/*
+// @match          http://*.ingress.com/mission/*
 // @grant          none 
 // ==/UserScript==
